d14b7a72425ac5926bbe4605d6d87ec9b65eda51
Harsh Bhamare <harshbhamaremit@gmail.com>
2026-02-10 12:07:35 +0530

    first 2 program

---
+++ 02_sumOfTwoNumbers/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter first number: ");
+        int a = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Enter second number: ");
+        int b = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Sum = " + (a + b));
+    }
+}
